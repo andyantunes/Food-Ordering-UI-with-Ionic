d025ab4cd16221371f2583be6315a0bd0bef2639
anderson.antunes <andersonlantunes@gmail.com>
2021-05-09 15:42:01 -0300

    Ionic slides

--- src/app/details/details.module.ts
+++ src/app/details/details.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
 
-import { HomePageRoutingModule } from './home-routing.module';
+import { IonicModule } from '@ionic/angular';
+
+import { DetailsPageRoutingModule } from './details-routing.module';
 import { SharedDirectivesModule } from '../directives/shared-directives.module';
+
+import { DetailsPage } from './details.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule,
+    DetailsPageRoutingModule,
     SharedDirectivesModule,
   ],
-  declarations: [HomePage],
+  declarations: [DetailsPage],
 })
-export class HomePageModule {}
+export class DetailsPageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  protected categories = [];
+  protected highlights = [];
+  protected featured = [];
 
-  constructor() {}
+  protected catSlideOpts = {
+    freeMode: true,
+    slidesPerView: 3.5,
+    slidesOffsetBefore: 11,
+    spaceBetween: 10,
+  };
 
+  protected highlightSlidesOpts = {
+    slidesPerView: 1.05,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+  };
+
+  protected featuredSlidesOpts = {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    freeMode: true,
+  };
+
+  private homeJson: string;
+
+  constructor(private http: HttpClient) {
+    this.homeJson =
+      'https://devdactic.fra1.digitaloceanspaces.com/foodui/home.json';
+  }
+
+  ngOnInit() {
+    this.http.get(this.homeJson).subscribe((res: any) => {
+      console.log(res);
+      this.categories = res.categories;
+      this.highlights = res.highlights;
+      this.featured = res.featured;
+    });
+  }
 }
